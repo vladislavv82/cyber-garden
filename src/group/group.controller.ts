fc13015bb6c0c48fb3ca55e9cc9865f23138f9ed
vladislavv82 <vladislav.akopian82@gmail.com>
2024-11-23 12:36:45 +0300

    hot fix:

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,34 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { GroupService } from './group.service';
-import { CreateGroupDto } from './dto/create-group.dto';
-import { UpdateGroupDto } from './dto/update-group.dto';
+// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// import { GroupService } from './group.service';
+// import { CreateGroupDto } from './dto/create-group.dto';
+// import { UpdateGroupDto } from './dto/update-group.dto';
+
+import { Controller } from "@nestjs/common";
+import { GroupService } from "./group.service";
 
 @Controller('group')
 export class GroupController {
   constructor(private readonly groupService: GroupService) {}
 
-  @Post()
-  create(@Body() createGroupDto: CreateGroupDto) {
-    return this.groupService.create(createGroupDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.groupService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.groupService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
-    return this.groupService.update(+id, updateGroupDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.groupService.remove(+id);
-  }
 }
--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
+import { PrismaService } from '@/prisma.service';
 
 @Module({
   controllers: [GroupController],
-  providers: [GroupService],
+  providers: [GroupService, PrismaService],
   exports: [GroupModule]
 })
 export class GroupModule {}
