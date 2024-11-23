8a19f585327bb57341314ba2aca344d155b1f645
vladislavv82 <vladislav.akopian82@gmail.com>
2024-11-23 12:40:48 +0300

    hot fix: v2

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,7 +1,4 @@
-// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-// import { GroupService } from './group.service';
-// import { CreateGroupDto } from './dto/create-group.dto';
-// import { UpdateGroupDto } from './dto/update-group.dto';
+
 
 import { Controller } from "@nestjs/common";
 import { GroupService } from "./group.service";
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { PrismaService } from '@/prisma.service'
 import { Module } from '@nestjs/common'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
+import { GroupService } from '@/group/group.service'
 
 @Module({
 	controllers: [UserController],
-	providers: [UserService, PrismaService],
+	providers: [UserService, PrismaService, GroupService],
 	exports: [UserService]
 })
 export class UserModule {}
